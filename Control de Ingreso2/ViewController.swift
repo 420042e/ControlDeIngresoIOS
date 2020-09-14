b13e39a8473357ad9d38d91e2a309929e86a4831
Esteban Choque Villalobos <esteban@iMac-de-Esteban.local>
2020-09-14 10:16:59 -0700

    Initial Commit

---
+++ Control de Ingreso2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Control de Ingreso2
+//
+//  Created by Esteban Choque Villalobos on 9/14/20.
+//  Copyright © 2020 Esteban Choque Villalobos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
