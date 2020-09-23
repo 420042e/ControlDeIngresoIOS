7b742318657c34944b3a1b7a545bfd5638d53e00
Esteban Choque Villalobos <esteban@iMac-de-Esteban.local>
2020-09-23 14:05:03 -0400

    Initial commit

---
+++ Control de Ingreso2/Model/Usuario.swift
@@ -0,0 +1,25 @@
+//
+//  Horario.swift
+//  Control de Ingreso2
+//
+//  Created by Esteban Choque Villalobos on 9/21/20.
+//  Copyright © 2020 Esteban Choque Villalobos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Usuario {
+    let username:String
+    let password:String
+    let email:String
+    let pic:String
+    let fullname:String
+    let occupation:String
+    let phone:String
+    let address:String
+    let state:String
+    let recinto:String
+    let rol:String
+}
+
